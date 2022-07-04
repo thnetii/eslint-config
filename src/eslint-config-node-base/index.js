1ea62476bff3bf21fc0ab77f7fc40763f6e9efb9
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2022-07-04 18:01:42 +0200

    Run prettier fixes

--- src/eslint-config-node-base/index.js
+++ src/eslint-config-node-base/index.js
@@ -5,15 +5,21 @@ const noRestrictedSyntax = rules['no-restricted-syntax'];
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
   plugins: ['node', 'prettier'],
-  extends: ['eslint:recommended', 'plugin:node/recommended', 'airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+  ],
   rules: {
     'no-restricted-syntax': [
       noRestrictedSyntax[0],
-      ...noRestrictedSyntax
-        .filter((r) => {
-          if (typeof r !== 'object') { return false; }
-          return r.selector !== 'ForOfStatement';
-        }),
+      ...noRestrictedSyntax.filter((r) => {
+        if (typeof r !== 'object') {
+          return false;
+        }
+        return r.selector !== 'ForOfStatement';
+      }),
     ],
   },
 };
